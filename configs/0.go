4c7b30cfb16576640869cfd03d8c1c4a4f266192
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-08-22 11:20:12 -0300

    Adds structure to hold the flags parameters.

--- configs/0.go
+++ configs/0.go
@@ -1,9 +1,39 @@
 package configs
 
-import "os"
+import (
+	"flag"
+	"os"
+)
+
+type Flag struct {
+	ShowPlates  bool
+	CreatePlan  bool
+	EnableEmail bool
+	Help        bool
+	ShowConfig  bool
+	Source      string
+}
+
+func (f *Flag) Parse() {
+	flag.BoolVar(&f.ShowPlates, "show-plates", false, "Shows plates available.")
+	flag.BoolVar(&f.CreatePlan, "create-plan", false, "Creates a new plan.")
+	flag.BoolVar(&f.EnableEmail, "enable-email", false, "Enables email sender.")
+	flag.BoolVar(&f.Help, "help", false, "Shows help.")
+	flag.BoolVar(&f.ShowConfig, "show-config", false, "Shows actual configuration.")
+	flag.StringVar(&f.Source, "source", "yaml", "Source of plates: database, yaml. Default: yaml")
+
+	if f.Source != "yaml" && f.Source != "database" {
+		panic("Invalid source: use yaml or database.")
+	}
+}
+
+func (f *Flag) ShowHelp() {
+	flag.PrintDefaults()
+}
 
 var environment string
 
+// IsProduction returns true the current environment is production.
 func IsProduction() bool {
 	return environment == "production"
 }
